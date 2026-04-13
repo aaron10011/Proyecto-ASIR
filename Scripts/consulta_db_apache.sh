0d4566b7359e1a7bd19019a379e16030fe04efec
Aaron Perez <aaronpr117@gmail.com>
2026-04-13 21:57:04 +0100

    Update consulta_db_apache.sh

--- Scripts/consulta_db_apache.sh
+++ Scripts/consulta_db_apache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mysql -h 192.168.2.20 -u monitor_zabbix -p'0408' -e "SELECT 1;" web_formularios >/dev/null 2>&1
+mysql -h 192.168.2.20 -u monitor_zabbix -p'CONTRASEÑA' -e "SELECT 1;" web_formularios >/dev/null 2>&1
 
 if [ $? -eq 0 ]; then
   echo 1
